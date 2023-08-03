95665f4f7f33e41cbf2be16b5fd3846ff033bb53
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-08-03 20:48:39 +0200

    Given classNames for the parts of AboutUs article. Added pisture for article background.

--- src/App.js
+++ src/App.js
@@ -39,5 +39,30 @@ function Menu() {
 }
 
 function AboutUs() {
-  return <article className="AboutUs">ABOUT US</article>;
+  return (
+    <article className="AboutUs">
+      <h2>ABOUT US</h2>
+      <main className="main">
+        <div className="border-radius">
+          <p className="main-part-one">
+            We are group of neighbours and friends that have have some nice
+            things in common. Primarely, we all live in the third part of
+            Skojevsko Naselje that is located in Municipality of Rakovica -
+            Belgrade, Serbia. Second, we all have big passion for cats and made
+            some budget to make their life in out neighbourhood the best
+            possible for any living cat.
+          </p>
+        </div>
+        <div className="border-radius">
+          <p className="main-part-two">
+            The main advantage of living in our neighbourhood is that we live
+            near the Košutnjak woods, and have lots of green fields and trees as
+            well. With the proper deeds from our group, slowly but steadily
+            other our neighbours started having affection towards cats, so we
+            were a huge part of that together with our amazing friends !
+          </p>
+        </div>
+      </main>
+    </article>
+  );
 }
