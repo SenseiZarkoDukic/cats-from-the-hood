ebc935c55585992b53e357a6d19ca489b37b10ed
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-08-06 17:23:47 +0200

    Setting up buttons for carousel using icons

--- src/App.js
+++ src/App.js
@@ -146,13 +146,12 @@ function CatsList() {
 function Cat({ cat, currCat, onCurrCat }) {
   return (
     <li className="cat-li">
-      <div className="cat-image-div">
-        <img
-          className="cat-image"
-          src={cat.image + cat.name + ".jpg"}
-          alt={cat.name}
-        />
-      </div>
+      <img
+        className="cat-image"
+        src={cat.image + cat.name + ".jpg"}
+        alt={cat.name}
+      />
+
       <h2 className="cat-headline">😸 My name is {cat.name}</h2>
       {cat.Father && (
         <p className="text-about-cat">
@@ -174,8 +173,38 @@ function Cat({ cat, currCat, onCurrCat }) {
       <p className="text-about-cat">
         <span className="no-italic">😻</span> {cat.description}
       </p>
-      <button className="btn btn--left"></button>
-      <button className="btn btn--left"></button>
+      <button className="btn btn--left">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+      </button>
+      <button className="btn btn--right">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.25 4.5l7.5 7.5-7.5 7.5"
+          />
+        </svg>
+      </button>
     </li>
   );
 }
