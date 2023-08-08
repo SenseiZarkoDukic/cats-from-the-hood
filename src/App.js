7fb2a3cbfc73077ce06b3cac91e8e1d1d3a9cac4
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-08-08 21:14:13 +0200

    Made Cat component as a new Cat.js file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import AboutUs from "./AboutUs";
+import CatsList from "./CatsList";
 
 const cats = [
   {
@@ -77,73 +78,6 @@ export default function App() {
         </Routes>
       </div>
     </Router>
-  );
-}
-
-function Cat({ cat, currCat, onCurrCat, onPrev, onNext, imageID }) {
-  return (
-    <li key={cat.id} className="cat-li">
-      <img
-        key={imageID}
-        className="cat-image"
-        src={cat.image + cat.name + ".jpg"}
-        alt={cat.name}
-      />
-
-      <h2 className="cat-headline">😸 My name is {cat.name}</h2>
-      {cat.Father && (
-        <p className="text-about-cat">
-          <span className="no-italic">🦁</span> My father's name is {cat.Father}
-          .
-        </p>
-      )}
-      {cat.Mother && (
-        <p className="text-about-cat">
-          <span className="no-italic">🐯</span> My Mother's name is {cat.Mother}
-          .
-        </p>
-      )}
-      <p className="text-about-cat">
-        <span className="no-italic">🐈‍⬛</span> The color of my fur is {cat.color}
-        .
-      </p>
-      <h3 className="cat-headline">😺 MORE ABOUT ME</h3>
-      <p className="text-about-cat">
-        <span className="no-italic">😻</span> {cat.description}
-      </p>
-      <button className="btn btn--left" onClick={onPrev}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6 btn-icon"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 19.5L8.25 12l7.5-7.5"
-          />
-        </svg>
-      </button>
-      <button className="btn btn--right" onClick={onNext}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6 btn-icon"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.25 4.5l7.5 7.5-7.5 7.5"
-          />
-        </svg>
-      </button>
-    </li>
   );
 }
 
