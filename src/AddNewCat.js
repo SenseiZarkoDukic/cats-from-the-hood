c1b28164dd258f7f96faa5895499458399b31d88
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-08-08 21:20:56 +0200

    Made AddNewCat component as a new AddNewCat.js file

---
+++ src/AddNewCat.js
@@ -0,0 +1,94 @@
+import { useState } from "react";
+
+export default function AddNewCat({
+  newIdNumber,
+  onNewIdNumber,
+  onAddNewCat,
+  allCats,
+}) {
+  const [name, setName] = useState("");
+  const [Father, setFather] = useState("");
+  const [Mother, setMother] = useState("");
+  const [color, setColor] = useState("");
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const catImage = "./Images/";
+
+    const imageID = crypto.randomUUID();
+    const id = allCats.length + 1;
+
+    if (!name || !Father || !Mother || !color || !description) return;
+    const newCat = {
+      name,
+      Father,
+      Mother,
+      color,
+      description,
+      image: `${catImage}`,
+      imageID: `${imageID}`,
+      id: `${id}`,
+    };
+    onAddNewCat(newCat);
+    console.log(newCat, id);
+  }
+  console.log(allCats.length);
+  return (
+    <div className="AddNewCat">
+      <h1 className="add-cat-headline">Add New Cat</h1>
+      <form className="addNewCatForm" onSubmit={handleSubmit}>
+        <label className="form-cats-entry">Enter cat's name: </label>
+        <input
+          className="form-input-field"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <label className="form-cats-entry">Enter cat's father name: </label>
+        <input
+          className="form-input-field"
+          type="text"
+          value={Father}
+          onChange={(e) => setFather(e.target.value)}
+        ></input>
+        <label className="form-cats-entry">Enter cat's mother name: </label>
+        <input
+          className="form-input-field"
+          type="text"
+          value={Mother}
+          onChange={(e) => setMother(e.target.value)}
+        ></input>
+        <label className="form-cats-entry">Enter cat's color: </label>
+        <input
+          className="form-input-field"
+          type="text"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        ></input>
+        <label className="form-cats-entry">
+          Enter decription of the cat:{" "}
+          <span className="min-length">(min. 50 characters)</span>{" "}
+        </label>
+        <textarea
+          minlength="50"
+          className="form-input-field form-description"
+          type="text"
+          cols="40"
+          rows="5"
+          name="Text1"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+        <button
+          className="btn--add-cat box-shadow-lighter"
+          on
+          onClick={() => handleSubmit}
+        >
+          Add
+        </button>
+      </form>
+    </div>
+  );
+}
